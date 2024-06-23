80f087687cd4ccafa4c19f987c9b0e964f3beb11
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-06-22 21:48:13 -0400

    kubctl yaml

--- README.md
+++ README.md
@@ -1,30 +1,4 @@
-To sync Event
-![Alt text](img/SyncingEvents.png)
 
-Output:
-
-Creating a new post
-![Alt text](img/Working.png)
-
-Turning of the query service
-When I turn of the query service the user is still able to enter the inputs:
-![Alt text](img/shutthequeryservice.png)
-
-Adding Comments:
-![Alt text](img/addingcommentsandposts.png)
-
-When we refresh:
-![Alt text](img/refresh.png)
-
-Turning the query service back up:
-![Alt text](img/syncingtheevents.png)
-
-
-Hence, We achived Sync, This the output from my terminal.
-![Alt text](img/TermianlSync.png)
-
-
-Next is to containerize it by using Docker.
 
 
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -12,18 +12,18 @@ app.post('/events',(req, res) => {
 
     events.push(event);
 
-    axios.post("http://localhost:4000/events", event).catch((err) => {
+    axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
       console.log(err.message + "Error Here 4000 ");
     });
-    axios.post("http://localhost:4001/events", event).catch((err) => {
-      console.log(err.message + "Error Here 4001");
-    });
-    axios.post("http://localhost:4002/events", event).catch((err) => {
-      console.log(err.message + "Error Here 4002");
-    });
-    axios.post("http://localhost:4003/events", event).catch((err) => {
-      console.log(err.message + "Error Here 4003");
-    });
+    // axios.post("http://localhost:4001/events", event).catch((err) => {
+    //   console.log(err.message + "Error Here 4001");
+    // });
+    // axios.post("http://localhost:4002/events", event).catch((err) => {
+    //   console.log(err.message + "Error Here 4002");
+    // });
+    // axios.post("http://localhost:4003/events", event).catch((err) => {
+    //   console.log(err.message + "Error Here 4003");
+    // });
     res.send({ status: "OK" });
 });
 
