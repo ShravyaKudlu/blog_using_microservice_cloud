bc027028edd447a545d2f5827e811757ab5e182c
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2024-06-23 20:32:19 -0400

    all yaml complete

--- moderation/index.js
+++ moderation/index.js
@@ -5,25 +5,26 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events',async (req,res) => {
-    const{type, data} = req.body;
-    if(type === 'CommentCreated'){
-        const status = data.content.includes('orange') ? 'rejected' : 'approved';
-    
-        await axios.post('http://localhost:4005/events',{
-            type: 'CommentModerated',
-            data:{
-                id: data.id,
-                postId: data.postId,
-                status,
-                content: data.content  
-            }
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
-        });
-    }
-    res.send({});
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+
+  res.send({});
 });
 
-app.listen(4003, ()=>{
-    console.log('Listening on 4003');
+app.listen(4003, () => {
+  console.log('Listening on 4003');
 });
